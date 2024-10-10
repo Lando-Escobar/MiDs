5039cd0956955191667ecb0d222a6a9dd29ce0fe
Lando-Escobar <razanakolonalando@gmail.com>
2024-10-10 09:35:56 +0200

    ajout district

--- application/views/districts_view.php
+++ application/views/districts_view.php
@@ -1,4 +1,11 @@
-<!-- application/views/districts_view.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LISTE DISTRICTS</title>
+</head>
+<body>
 <h2>Districts de la région <?= $region_name; ?></h2>
 <ul>
     <?php if (!empty($districts)): ?>
@@ -9,11 +16,13 @@
                 </a> 
                 
             </li>
-            
         <?php endforeach; ?>
         <br><nav><a href="<?= site_url('location/regions'); ?>">Retour à la liste des régions</a></nav>
     <?php else: ?>
         <li>Aucun district trouvé pour cette région.</li>
         <a href="<?= site_url('location/regions'); ?>">Retour à la liste des régions</a>
     <?php endif; ?>
-</ul>
+</ul> 
+</body>
+</html>
+
